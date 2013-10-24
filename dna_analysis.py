6f5ede0def169c6b6ee107543bf2eac3834f4f14
schmat18 <schultmatthew@gmail.com>
2013-10-24 12:35:32 -0700

    Homework 3
    Completed

--- dna_analysis.py
+++ dna_analysis.py
@@ -17,6 +17,11 @@
 import sys
 
 
+#---------------------------------------------------------------------------------
+# cmd cd to this file then type "python dna_analysis.py data\\sample_1.fasq"
+#---------------------------------------------------------------------------------
+
+
 ###########################################################################
 ### Read the nucleotides into a variable named seq
 ###
@@ -55,19 +60,74 @@ total_count = 0
 gc_count = 0
 
 
+#####My Work######
+
+at_count=0
+gCount=0
+cCount=0
+tCount=0
+aCount=0
+sumCount=0
+totalCount=0
+seqLength=0
+atcgRatio=0.0
+gcClassification=""
+##################
+
+
 # for each base pair in the string,
 for bp in seq:
     # increment the total number of bps we've seen
     total_count = total_count + 1
 
     # next, if the bp is a G or a C,
-    if bp == 'C' or bp == 'G':
+    #if bp == 'C' or bp == 'G':
         # increment the count of gc
-        gc_count = gc_count + 1
+        #gc_count = gc_count + 1
+    if bp =='C':
+        cCount = cCount+1
+        
+    elif bp=='G':
+        gCount =gCount+1
+        
+    elif bp=='T':
+        tCount=tCount+1
+        
+    elif bp=='A':
+        aCount=aCount+1
 
 
+at_count= (aCount + tCount)
+gc_count= (gCount + cCount)
 # divide the gc_count by the total_count
 gc_content = float(gc_count) / total_count
 
+
+at_content = float(at_count) / total_count
+
+sumCount= (gCount + cCount + tCount + aCount)
+
+atcgRatio= float(at_content/gc_content)
+
+seqLength=total_count
+if(gc_content >0.6):
+    gcClassification = "High GC Classification"
+elif(gc_content> 0.4):
+    gcClassification = "Moderate GC Classification"
+else:
+    gcClassification = "Low GC Classification"
+    
 # Print the answer
 print 'GC-content:', gc_content
+print 'AT-content:',at_content
+print 'gCount:',gCount
+print 'cCount:',cCount
+print 'tCount:',tCount
+print 'aCount:',aCount
+#print 'cgCount:',gc_count
+#print 'atCount:',at_count
+print 'sumCount:',sumCount
+print 'totalCount:',total_count
+print 'seqLength:',seqLength
+print 'at/cgRatio:',atcgRatio
+print 'gcClassification:',gcClassification
