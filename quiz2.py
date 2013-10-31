78b5f47d5d2593995e11cef1ce201b0ca5c4e2e2
schmat18 <schultmatthew@gmail.com>
2013-10-31 11:19:22 -0700

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -1,9 +1,9 @@
 #from a module called lab2
 #import a variable called n
 #print one of the following things:
-# "greater than 50"
-# "less than 50"
-# "equal to 50"
+#  "greater than 50"
+#  "less than 50"
+#  "equal to 50"
 # Based on the value of n
 
 from lab2 import n
